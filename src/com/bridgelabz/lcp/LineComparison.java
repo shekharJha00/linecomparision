343f3e8e6caec521e7239f8186b15bc37152f125
shekhar <shekharjha7417870578@gmail.com>
2022-08-05 22:21:48 +0530

    uc3 added

--- src/com/bridgelabz/lcp/LineComparison.java
+++ src/com/bridgelabz/lcp/LineComparison.java
@@ -7,6 +7,7 @@ public class LineComparison {
 
     public static void main(String[] args) {
         System.out.println("Welcome to The Line Comparison Computation Program");
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter x1 of First Point: ");
         int x1 = scanner.nextInt();
@@ -26,19 +27,21 @@ public class LineComparison {
         System.out.println("Enter y2 of Forth Point: ");
         int q2 = scanner.nextInt();
         scanner.close();
+
         Double lenghtOfLine1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         Double lenghtOfLine2 = Math.sqrt(Math.pow(p2 - p1, 2) + Math.pow(q2 - q1, 2));
 
         System.out.printf("Length of First Line is : %.2f %n", lenghtOfLine1);
         System.out.printf("Length of Second Line is : %.2f %n", lenghtOfLine2);
 
-        boolean ans = lenghtOfLine1.equals(lenghtOfLine2);
-        if (ans) {
-            System.out.println("Both Lines are Equal");
+        int result = lenghtOfLine1.compareTo(lenghtOfLine2);
+        if (result > 0) {
+            System.out.println("Length of First end points is Greater than Length of Second end Points");
+        } else if (result < 0) {
+            System.out.println("Length of First end points is Less than Length of Second end Points");
         } else {
-            System.out.println("Both Lines are not Equal ");
+            System.out.println("Both Lines are Equal ");
         }
 
 
-
-
+    }}
